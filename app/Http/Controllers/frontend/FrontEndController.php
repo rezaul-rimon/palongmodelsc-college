7d879bb161dcd9b536bd93af41fd299a827ab143
rezaul-rimon <rezaul0495@gmail.com>
2023-11-12 00:12:49 +0600

    class division

--- app/Http/Controllers/frontend/FrontEndController.php
+++ app/Http/Controllers/frontend/FrontEndController.php
@@ -24,7 +24,167 @@ class FrontEndController extends Controller
             ->orderBy('event_date') // Order events by event date in ascending order
             ->get();
 
+        //Class 6 to 8
+        // $class_6_to_8 = Students::where('status', 1)
+        //     ->whereIn('class_name', [6, 7, 8])
+        //     ->get();
+        
+        $class_6 = [
+            'male' => 0,
+            'female' => 0,
+        ];
+        $class_7 = [
+            'male' => 0,
+            'female' => 0,
+        ];
+        $class_8 = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_9_sc = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_10_sc = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_9_com = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_10_com = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_9_ar = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_10_ar = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_9_comp = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_10_comp = [
+            'male' => 0,
+            'female' => 0,
+        ];
+        
+        $class_9_cv = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        $class_10_cv = [
+            'male' => 0,
+            'female' => 0,
+        ];
+
+        foreach ($students as $items) {
+            if ($items->class_name == 6) {
+                $class_6['male'] += $items->male_students;
+                $class_6['female'] += $items->female_students;
+            }
+        
+            if ($items->class_name == 7) {
+                $class_7['male'] += $items->male_students;
+                $class_7['female'] += $items->female_students;
+            }
+        
+            if ($items->class_name == 8) {
+                $class_8['male'] += $items->male_students;
+                $class_8['female'] += $items->female_students;
+            }
+
+            if($items->class_name == 9){
+                if($items->class_section == 'A'){
+                    $class_9_sc['male'] += $items->male_students;
+                    $class_9_sc['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'B' or $items->class_section == 'C'){
+                    $class_9_com['male'] += $items->male_students;
+                    $class_9_com['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'D'){
+                    $class_9_ar['male'] += $items->male_students;
+                    $class_9_ar['female'] += $items->female_students;
+                }
+                
+                else if($items->class_section == 'Computer'){
+                    $class_9_comp['male'] += $items->male_students;
+                    $class_9_comp['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'Civil'){
+                    $class_9_cv['male'] += $items->male_students;
+                    $class_9_cv['female'] += $items->female_students;
+                }
+            }
+
+            if($items->class_name == 10){
+                if($items->class_section == 'A'){
+                    $class_10_sc['male'] += $items->male_students;
+                    $class_10_sc['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'B' or $items->class_section == 'C'){
+                    $class_10_com['male'] += $items->male_students;
+                    $class_10_com['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'D'){
+                    $class_10_ar['male'] += $items->male_students;
+                    $class_10_ar['female'] += $items->female_students;
+                }
+                
+                else if($items->class_section == 'Computer'){
+                    $class_10_comp['male'] += $items->male_students;
+                    $class_10_comp['female'] += $items->female_students;
+                }
+
+                else if($items->class_section == 'Civil'){
+                    $class_10_cv['male'] += $items->male_students;
+                    $class_10_cv['female'] += $items->female_students;
+                }
+            }
+        }
+        //dd($class_10_comp);
+        //dd($class_6, $class_7, $class_8);
+        
         //dd($committee);
-        return view('frontend.index', compact('committee', 'students', 'stipend_students', 'notices', 'events'));
+        return view('frontend.index', compact(
+            'committee',
+            'students',
+            'stipend_students',
+            'notices',
+            'events',
+            'class_6',
+            'class_7',
+            'class_8',
+            'class_9_sc',
+            'class_9_com',
+            'class_9_ar',
+            'class_9_comp',
+            'class_9_cv',
+            'class_10_sc',
+            'class_10_com',
+            'class_10_ar',
+            'class_10_comp',
+            'class_10_cv',
+        ));
     }
 }
--- resources/views/frontend/layouts/class_division.blade.php
+++ resources/views/frontend/layouts/class_division.blade.php
@@ -1,237 +1,246 @@
 <section class="ftco-section ftco-no-pb">
-		<div class="container">
-			<div class="row justify-content-center mb-5 pb-2">
-				<div class="col-md-8 text-center heading-section ftco-animate">
-					<h2 class="mb-4"><span>আমাদের</span> শেণী বিভাজন</h2>
-					<p>Separated they live in. A small river named Duden flows by their place and supplies it with the
-						necessary regelialia. It is a paradisematic country</p>
+	@php
+		use App\Helpers\AppHelper;
+	@endphp
+
+	<div class="container">
+		<div class="row justify-content-center mb-5 pb-2">
+			<div class="col-md-8 text-center heading-section ftco-animate">
+				<h2 class="mb-4"><span>আমাদের</span> শেণী বিভাজন</h2>
+				<p>Separated they live in. A small river named Duden flows by their place and supplies it with the
+					necessary regelialia. It is a paradisematic country</p>
+			</div>
+		</div>
+		<div class="row">
+
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/science.png);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">বিজ্ঞান বিভাগ</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<tr>
+								<th>শ্রেণী</th>
+								<th>ছাত্র</th>
+								<th>ছাত্রী</th>
+								<th>মোট</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>১০ম</td>
+								<td>{{ AppHelper::en2bn($class_10_sc['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_10_sc['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_10_sc['male'] + $class_10_sc['female']) }}</th>
+							</tr>
+							<tr>
+								<td>৯ম</td>
+								<td>{{ AppHelper::en2bn($class_9_sc['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_9_sc['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_9_sc['male'] + $class_9_sc['female']) }}</th>
+							</tr>
+						</tbody>
+						<tfoot>
+							<th>মোট</th>
+							<td>{{ AppHelper::en2bn($class_10_sc['male'] + $class_9_sc['male']) }}</td>
+							<td>{{ AppHelper::en2bn($class_10_sc['female'] + $class_9_sc['female']) }}</td>
+							<th>{{ AppHelper::en2bn($class_10_sc['male'] + $class_9_sc['male'] + $class_10_sc['female'] + $class_9_sc['female']) }}</th>
+						</tfoot>						
+					</table>
 				</div>
 			</div>
-			<div class="row">
-
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/science.png);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">বিজ্ঞান বিভাগ</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<tr>
-									<th>শ্রেণী</th>
-									<th>ছাত্র</th>
-									<th>ছাত্রী</th>
-									<th>মোট</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>১০ম</td>
-									<td>৬</td>
-									<td>১৩</td>
-									<th>১৯</th>
-								</tr>
-								<tr>
-									<td>৯ম</td>
-									<td>৭</td>
-									<td>২৬</td>
-									<th>৩৩</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>১৩</th>
-								<th>৩৯</th>
-								<th>৪৬</th>
-							</tfoot>
-						</table>
-					</div>
+			
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/business.png);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">ব্যাবসায় শিক্ষা</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<th>শ্রেণী</th>
+							<th>ছাত্র</th>
+							<th>ছাত্রী</th>
+							<th>মোট</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>১০ম</td>
+								<td>{{ AppHelper::en2bn($class_10_com['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_10_com['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_10_com['male'] + $class_10_com['female']) }}</th>
+							</tr>
+							<tr>
+								<td>৯ম</td>
+								<td>{{ AppHelper::en2bn($class_9_com['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_9_com['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_9_com['male'] + $class_9_com['female']) }}</th>
+							</tr>
+						</tbody>
+						<tfoot>
+							<th>মোট</th>
+							<td>{{ AppHelper::en2bn($class_10_com['male'] + $class_9_com['male']) }}</td>
+							<td>{{ AppHelper::en2bn($class_10_com['female'] + $class_9_com['female']) }}</td>
+							<th>{{ AppHelper::en2bn($class_10_com['male'] + $class_9_com['male'] + $class_10_com['female'] + $class_9_com['female']) }}</th>
+						</tfoot>
+						
+					</table>
 				</div>
-				
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/business.png);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">ব্যাবসায় শিক্ষা</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<th>শ্রেণী</th>
-								<th>ছাত্র</th>
-								<th>ছাত্রী</th>
-								<th>মোট</th>
-							</thead>
-							<tbody>
-								<tr>
-									<td>১০ম</td>
-									<td>৬</td>
-									<td>১৩</td>
-									<th>১৯</th>
-								</tr>
-								<tr>
-									<td>৯ম</td>
-									<td>৭</td>
-									<td>২৬</td>
-									<th>৩৩</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>১৩</th>
-								<th>৩৯</th>
-								<th>৪৬</th>
-							</tfoot>
-						</table>
-					</div>
-				</div>
+			</div>
 
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/arts.png);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">মানবিক বিভাগ</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<th>শ্রেণী</th>
-								<th>ছাত্র</th>
-								<th>ছাত্রী</th>
-								<th>মোট</th>
-							</thead>
-							<tbody>
-								<tr>
-									<td>১০ম</td>
-									<td>৬</td>
-									<td>১৩</td>
-									<th>১৯</th>
-								</tr>
-								<tr>
-									<td>৯ম</td>
-									<td>৭</td>
-									<td>২৬</td>
-									<th>৩৩</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>১৩</th>
-								<th>৩৯</th>
-								<th>৪৬</th>
-							</tfoot>
-						</table>
-					</div>
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/arts.png);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">মানবিক বিভাগ</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<th>শ্রেণী</th>
+							<th>ছাত্র</th>
+							<th>ছাত্রী</th>
+							<th>মোট</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>১০ম</td>
+								<td>{{ AppHelper::en2bn($class_10_ar['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_10_ar['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_10_ar['male'] + $class_10_ar['female']) }}</th>
+							</tr>
+							<tr>
+								<td>৯ম</td>
+								<td>{{ AppHelper::en2bn($class_9_ar['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_9_ar['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_9_ar['male'] + $class_9_ar['female']) }}</th>
+							</tr>
+						</tbody>
+						<tfoot>
+							<th>মোট</th>
+							<td>{{ AppHelper::en2bn($class_10_ar['male'] + $class_9_ar['male']) }}</td>
+							<td>{{ AppHelper::en2bn($class_10_ar['female'] + $class_9_ar['female']) }}</td>
+							<th>{{ AppHelper::en2bn($class_10_ar['male'] + $class_9_ar['male'] + $class_10_ar['female'] + $class_9_ar['female']) }}</th>
+						</tfoot>
+						
+					</table>
 				</div>
+			</div>
 
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/civil.png);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">সিভিল কন্সট্রাকশন (ভোকেশনাল)</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<th>শ্রেণী</th>
-								<th>ছাত্র</th>
-								<th>ছাত্রী</th>
-								<th>মোট</th>
-							</thead>
-							<tbody>
-								<tr>
-									<td>১০ম</td>
-									<td>৬</td>
-									<td>১৩</td>
-									<th>১৯</th>
-								</tr>
-								<tr>
-									<td>৯ম</td>
-									<td>৭</td>
-									<td>২৬</td>
-									<th>৩৩</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>১৩</th>
-								<th>৩৯</th>
-								<th>৪৬</th>
-							</tfoot>
-						</table>
-					</div>
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/civil.png);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">সিভিল কন্সট্রাকশন (ভোকেশনাল)</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<th>শ্রেণী</th>
+							<th>ছাত্র</th>
+							<th>ছাত্রী</th>
+							<th>মোট</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>১০ম</td>
+								<td>{{ AppHelper::en2bn($class_10_cv['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_10_cv['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_10_cv['male'] + $class_10_cv['female']) }}</th>
+							</tr>
+							<tr>
+								<td>৯ম</td>
+								<td>{{ AppHelper::en2bn($class_9_cv['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_9_cv['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_9_cv['male'] + $class_9_cv['female']) }}</th>
+							</tr>
+						</tbody>
+						<tfoot>
+							<th>মোট</th>
+							<td>{{ AppHelper::en2bn($class_10_cv['male'] + $class_9_cv['male']) }}</td>
+							<td>{{ AppHelper::en2bn($class_10_cv['female'] + $class_9_cv['female']) }}</td>
+							<th>{{ AppHelper::en2bn($class_10_cv['male'] + $class_9_cv['male'] + $class_10_cv['female'] + $class_9_cv['female']) }}</th>
+						</tfoot>
+					</table>
 				</div>
+			</div>
 
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/it_suppoer.png);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">আইটি সাপোর্ট এন্ড আইওটি বেসিক (ভোকেশনাল)</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<th>শ্রেণী</th>
-								<th>ছাত্র</th>
-								<th>ছাত্রী</th>
-								<th>মোট</th>
-							</thead>
-							<tbody>
-								<tr>
-									<td>১০ম</td>
-									<td>৬</td>
-									<td>১৩</td>
-									<th>১৯</th>
-								</tr>
-								<tr>
-									<td>৯ম</td>
-									<td>৭</td>
-									<td>২৬</td>
-									<th>৩৩</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>১৩</th>
-								<th>৩৯</th>
-								<th>৪৬</th>
-							</tfoot>
-						</table>
-					</div>
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/dept/it_suppoer.png);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">আইটি সাপোর্ট এন্ড আইওটি বেসিক (ভোকেশনাল)</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<th>শ্রেণী</th>
+							<th>ছাত্র</th>
+							<th>ছাত্রী</th>
+							<th>মোট</th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>১০ম</td>
+								<td>{{ AppHelper::en2bn($class_10_comp['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_10_comp['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_10_comp['male'] + $class_10_comp['female']) }}</th>
+							</tr>
+							<tr>
+								<td>৯ম</td>
+								<td>{{ AppHelper::en2bn($class_9_comp['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_9_comp['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_9_comp['male'] + $class_9_comp['female']) }}</th>
+							</tr>
+						</tbody>
+						<tfoot>
+							<th>মোট</th>
+							<td>{{ AppHelper::en2bn($class_10_comp['male'] + $class_9_comp['male']) }}</td>
+							<td>{{ AppHelper::en2bn($class_10_comp['female'] + $class_9_comp['female']) }}</td>
+							<th>{{ AppHelper::en2bn($class_10_comp['male'] + $class_9_comp['male'] + $class_10_comp['female'] + $class_9_comp['female']) }}</th>
+						</tfoot>
+					</table>
 				</div>
+			</div>
 
-				<div class="col-md-6 course d-lg-flex ftco-animate">
-					<div class="img" style="background-image: url({{ asset('frontend') }}/images/course-2.jpg);"></div>
-					<div class="text bg-light p-4">
-						<h3><a href="#">৬ষ্ঠ থেকে ৮ম শ্রেণী</a></h3>
-						<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
-						<table class="table table-bordered text-center student-table">
-							<thead>
-								<th>শ্রেণী</th>
-								<th>ছাত্র</th>
-								<th>ছাত্রী</th>
-								<th>মোট</th>
-							</thead>
-							<tbody>
-								<tr>
-									<td>৬ষ্ঠ</td>
-									<td>২০৬</td>
-									<td>২৩০</td>
-									<th>৪৩৭</th>
-								</tr>
-								<tr>
-									<td>৭ম</td>
-									<td>১৯৩</td>
-									<td>২১৯</td>
-									<th>৪১২</th>
-								</tr>
-								<tr>
-									<td>৮ম</td>
-									<td>১৪৩</td>
-									<td>২০৩</td>
-									<th>৩৪৬</th>
-								</tr>
-							</tbody>
-							<tfoot>
-								<th>মোট</th>
-								<th>৫৪২</th>
-								<th>৬৫২</th>
-								<th>১১৯৫</th>
-							</tfoot>
-						</table>
-					</div>
+			<div class="col-md-6 course d-lg-flex ftco-animate">
+				<div class="img" style="background-image: url({{ asset('frontend') }}/images/course-2.jpg);"></div>
+				<div class="text bg-light p-4">
+					<h3><a href="#">৬ষ্ঠ থেকে ৮ম শ্রেণী</a></h3>
+					<p class="subheading"><span>ক্লাস টাইম:</span> সকাল ১০টা - বিকাল ৪টা</p>
+					<table class="table table-bordered text-center student-table">
+						<thead>
+							<th>শ্রেণী</th>
+							<th>ছাত্র</th>
+							<th>ছাত্রী</th>
+							<th>মোট</th>
+						</thead>
+
+						<tbody>
+							<tr>
+								<td>{{ AppHelper::en2bn('৬ষ্ঠ') }}</td>
+								<td>{{ AppHelper::en2bn($class_6['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_6['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_6['male'] + $class_6['female']) }}</th>
+							</tr>
+							<tr>
+								<td>{{ AppHelper::en2bn('৭ম') }}</td>
+								<td>{{ AppHelper::en2bn($class_7['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_7['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_7['male'] + $class_7['female']) }}</th>
+							</tr>
+							<tr>
+								<td>{{ AppHelper::en2bn('৮ম') }}</td>
+								<td>{{ AppHelper::en2bn($class_8['male']) }}</td>
+								<td>{{ AppHelper::en2bn($class_8['female']) }}</td>
+								<th>{{ AppHelper::en2bn($class_8['male'] + $class_8['female']) }}</th>
+							</tr>
+						</tbody>
+						
+						<tfoot>
+							<th>{{ AppHelper::en2bn('মোট') }}</th>
+							<th>{{ AppHelper::en2bn($class_6['male'] + $class_7['male'] + $class_8['male']) }}</th>
+							<th>{{ AppHelper::en2bn($class_6['female'] + $class_7['female'] + $class_8['female']) }}</th>
+							<th>{{ AppHelper::en2bn($class_6['male'] + $class_7['male'] + $class_8['male'] + $class_6['female'] + $class_7['female'] + $class_8['female']) }}</th>
+						</tfoot>
+						
+					</table>
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
